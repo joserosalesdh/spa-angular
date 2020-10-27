c07dbaf3ad0055e045f618735ff853d5ba3eab15
joserosalesdh <jm.rosales92@gmail.com>
2020-10-27 17:55:31 -0300

    service heroe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 //Rutas
 import { APP_ROUTING } from './app.routes';
 
+//Service
+import { HeroesService } from './servicios/heroes.service';
+
 //Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/shared/navbar/navbar.component';
@@ -23,7 +26,9 @@ import { HeroesComponent } from './components/heroes/heroes.component';
     BrowserModule,
     APP_ROUTING
   ],
-  providers: [],
+  providers: [
+    HeroesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
